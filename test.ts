ff4bb05593003e67b1567a163dcd4e341c60d5c2
Jerem <jersou@gmail.com>
2022-05-05 07:48:30 +0200

    init

--- test.ts
+++ test.ts
@@ -2,9 +2,13 @@
 
 import {
   readerFromStreamReader,
-} from "https://deno.land/std@0.137.0/streams/conversion.ts";
+} from "https://deno.land/std@0.130.0/streams/conversion.ts";
 import { SAXParser } from "https://deno.land/x/xmlp@v0.2.9/mod.ts";
 
+
 const file = await Deno.open("0.xml");
-const inputReader = await readerFromStreamReader(file.readable.getReader());
+const inputReader = await readerFromStreamReader(file.readable
+      .pipeThrough(new TextDecoderStream())
+      .pipeThrough(new TextEncoderStream())
+.getReader());
 await new SAXParser().parse(inputReader);
