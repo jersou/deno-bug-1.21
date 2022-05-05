3f23681f1bb2021c4b31851bbf5c5d332874a2be
Jerem <jersou@gmail.com>
2022-05-05 22:57:12 +0200

    add vendors & deno canary test

--- test.ts
+++ test.ts
@@ -2,13 +2,15 @@
 
 import {
   readerFromStreamReader,
-} from "https://deno.land/std@0.130.0/streams/conversion.ts";
+} from "https://deno.land/std@0.138.0/streams/conversion.ts";
 import { SAXParser } from "https://deno.land/x/xmlp@v0.2.9/mod.ts";
 
-
 const file = await Deno.open("0.xml");
-const inputReader = await readerFromStreamReader(file.readable
-      .pipeThrough(new TextDecoderStream())
-      .pipeThrough(new TextEncoderStream())
-.getReader());
+
+const inputReader = await readerFromStreamReader(
+  file.readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextEncoderStream())
+    .getReader(),
+);
 await new SAXParser().parse(inputReader);
