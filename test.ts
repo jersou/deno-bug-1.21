abc7836fbfa7c14e69f296c63dd24bee747da452
Jerem <jersou@gmail.com>
2022-05-07 20:26:15 +0200

    fix: update xmlp@v0.2.9 → xmlp@v0.3.0

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@
 import {
   readerFromStreamReader,
 } from "https://deno.land/std@0.138.0/streams/conversion.ts";
-import { SAXParser } from "https://deno.land/x/xmlp@v0.2.9/mod.ts";
+import { SAXParser } from "https://deno.land/x/xmlp@v0.3.0/mod.ts";
 
 const file = await Deno.open("0.xml");
 
